2eac44bfed723c0cbf6816643d9239a7748dcfb6
josueredondo <josue.redondo2@gmail.com>
2026-01-22 10:18:26 -0600

    fix

--- app/src/main/java/com/devlosoft/megaposmobile/presentation/login/LoginState.kt
+++ app/src/main/java/com/devlosoft/megaposmobile/presentation/login/LoginState.kt
@@ -1,8 +1,8 @@
 package com.devlosoft.megaposmobile.presentation.login
 
 data class LoginState(
-    val userCode: String = "304720192",
-    val password: String = "304720192",
+    val userCode: String = "",
+    val password: String = "",
     val isLoading: Boolean = false,
     val isPasswordVisible: Boolean = false,
     val error: String? = null,
